b6c3919e2c27bb63db64f229957f47005596a68b
Валентина Терентьева <aboutall3@yandex.ru>
2022-09-04 18:41:30 +0300

    fix

---
+++ src/main/java/CashbackHacker.java
@@ -0,0 +1,9 @@
+package ru.netology.service;
+
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public CashbackHackService() { /* compiled code */ }
+
+    public int remain(int amount) { /* compiled code */ }
+}
